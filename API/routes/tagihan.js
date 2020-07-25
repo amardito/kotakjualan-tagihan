ffb8800e815122c1744dd5dec24b6ea472a8db00
amardito <amardito37@gmail.com>
2020-07-25 15:54:46 +0700

    get patch

--- API/routes/tagihan.js
+++ API/routes/tagihan.js
@@ -1,32 +1,39 @@
 const express = require('express');
 const router = express.Router();
-const Bill = require('../model/bill')
+const Bill = require('../model/bill');
+const bill = require('../model/bill');
 
 router.post('/', async(req, res, next) => {
     const bill = new Bill({
-        idToko : req.body.idToko,
+        namaToko : req.body.idToko,
         idAnggota : req.body.idAnggota,
         createdAt : new Date(),
         item : req.body.item,
         status : "true",
         totalTagihan : req.body.totalTagihan
-    })
+    });
     await bill.save();
     res.status(201).json({
         created : bill
-    })
+    });
 });
 
 router.get('/', async(req, res, next) => {
-    await Bill.find()
-    .select("idToko idAnggota item status totalTagihan")
-    .then(doc => {
-        res.status(200).json(doc);
-    });
-})
+    res.status(200).json(
+        await Bill.find({status : true})
+        .select("namaToko idAnggota item status totalTagihan")
+
+    );
+});
+
+router.get('/:idAnggota', async(req, res, next) => {
+    res.status(200).json(
+        await bill.find({idAnggota : req.params.idAnggota})
+    );
+});
 
 router.put('/lunas/:tagihan', async(req, res, next) => {
-    await Bill.updateOne({_id : req.params.tagihan}, {$set : {status : "false"}})
-    res.status(200).send({updating : "status to false"});
-})
+    await Bill.updateOne({_id : req.params.tagihan}, {$set : {status : "false"}});
+    res.status(201).send({updating : "status to false"});
+});
 module.exports = router;
